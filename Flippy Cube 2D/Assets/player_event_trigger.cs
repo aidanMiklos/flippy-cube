e89a436372ba7499b468c99d7ca3314333e596fb
Aidan Miklos <aidaniiimiklos@gmail.com>
2022-06-08 22:26:48 -0400

    Win and Lose Logic

---
+++ Flippy Cube 2D/Assets/player_event_trigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_event_trigger : MonoBehaviour
+{
+    public string bottomBlock;
+    public string topBlock;
+
+    void Update(){
+        bottomBlock = gameObject.GetComponent<player_movement>().bottomBlock;
+        topBlock = gameObject.GetComponent<player_movement>().topBlock;
+
+        if(bottomBlock == "AIR" || topBlock == "AIR"){
+            Die();
+        }
+        if(bottomBlock == "WIN" && topBlock == "WIN"){
+            Win();
+        }
+    }
+
+    public void Die(){
+        Debug.Log("DIE");
+    }
+    public void Win(){
+        Debug.Log("WIN");
+    }
+}
